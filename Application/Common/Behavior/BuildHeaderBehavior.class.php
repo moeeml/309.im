dca9e54619e1838cc2e84ed21f51c5f68efc4f0e
moeeml <xierungui@hotmail.com>
2014-12-02 10:22:45 +0800

    Modify origin build header

--- Application/Common/Behavior/BuildHeaderBehavior.class.php
+++ Application/Common/Behavior/BuildHeaderBehavior.class.php
@@ -8,14 +8,7 @@ class BuildHeaderBehavior{
 
     public function run()
     {
-    	$referer = parse_url($_SERVER['HTTP_REFERER']);
-    	$origin = $referer['host'];
-
-    	if(!empty($referer['port'])){
-    		$origin .= ':' . $referer['port'];
-    	}
-
-        header('Access-Control-Allow-Origin:'.$origin);
+        header('Access-Control-Allow-Origin:'.$_SERVER['HTTP_ORIGIN']);
         header('Access-Control-Allow-Credentials:true');
         header('Access-Control-Allow-Methods:POST, GET, OPTIONS');
         header('Access-Control-Allow-Headers:XML-Requested-With');
